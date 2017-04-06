34d1c3f7d22a08d4b8b3bce63fab07f87bc8c7e8
Juan Dalmasso <juan.dalmasso@eyemagnet.com>
2017-04-06 15:49:08 +1200

    Fixed Typo

--- app/src/controllers/aboutController.js
+++ app/src/controllers/aboutController.js
@@ -2,45 +2,14 @@
 
 /**
  * @ngdoc function
- * @name ConcertApp.controller:HomeController
+ * @name ConcertApp.controller:AboutController
  * @description
- * # HomeController
+ * # AboutController
  */
 module.exports = [
     '$scope',
-    '$timeout',
-    'SearchDataService',
 
-    function($scope, $timeout, $searchDataService)
+    function($scope)
     {
-        $scope.searchKeyword = '';
-        $scope.searchResults = null;
-        $scope.showLoading = false;
-
-        var searchDelayTimeout;
-
-        $scope.$watch('searchKeyword', function (val) {
-
-            if (searchDelayTimeout) {
-                $timeout.cancel(searchDelayTimeout)
-            }
-
-            searchDelayTimeout = $timeout(function() {
-                $scope.showLoading = true;
-                $scope.executeSearch();
-            }, 500);
-        })
-
-        $scope.executeSearch = function() {
-            if ($scope.searchKeyword.length >= 3) {
-                $scope.searchResults = [];
-                $searchDataService.search($scope.searchKeyword).then(function(data) {
-                    $scope.searchResults = data;
-                    $scope.showLoading = false;
-                });
-            } else {
-                $scope.searchResults = null;
-            }
-        }
     }
 ];
